b744758c5220ca5f662ecd995534d8ee4d3789f0
이태희 <leetaehee0205@gmail.com>
2021-08-10 21:26:07 +0900

    create useInput custom hooks

--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,20 +1,14 @@
 import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
 import React, { useCallback, useState } from 'react';
 
+import useInput from '@hooks/useInput';
+
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
+  const [email, onChangeEmail] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, , setPassword] = useInput("");
+  const [passwordCheck, , setPasswordCheck] = useInput("");
   const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, [nickname]);
 
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
