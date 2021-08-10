476ed0955bf92689ed9f8998a55a3c2a60df30f6
이태희 <leetaehee0205@gmail.com>
2021-08-10 21:58:23 +0900

    create login page

--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
 import React, { useCallback, useState } from 'react';
 
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import useInput from '@hooks/useInput';
 
@@ -93,7 +94,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
